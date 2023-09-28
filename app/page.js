fd8a14dfc9181402356a5db4d71eee96ea6c073e
Heba-Webdev23 <jtvonline@yahoo.com>
2023-09-29 01:33:21 +0300

    Created the Home comonent

--- app/page.js
+++ app/page.js
@@ -1,10 +1,9 @@
-import Navbar from "@/components/globals/Navbar";
+import Home from "@/components/home/Home";
 
-export default function Home() {
+export default function App() {
   return (
-    <main>
-      <Navbar />
-      <h1>hello</h1>
-    </main>
+    <>
+      <Home />
+    </>
   );
 }
