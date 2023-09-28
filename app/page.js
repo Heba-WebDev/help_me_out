44381667187a7b40c72bbc5d66c984de2896c7bb
Heba-Webdev23 <jtvonline@yahoo.com>
2023-09-29 01:28:31 +0300

    Added Navbar component

--- app/page.js
+++ app/page.js
@@ -1,8 +1,9 @@
-import Image from "next/image";
+import Navbar from "@/components/globals/Navbar";
 
 export default function Home() {
   return (
     <main>
+      <Navbar />
       <h1>hello</h1>
     </main>
   );
